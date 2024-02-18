94fd2e066a75756378269a77ebfe32dc5337ee89
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-18 17:26:44 +0330

    fix static urls configuration

--- manage_party_service/urls.py
+++ manage_party_service/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from manage_party_service.settings.base import get_environment_var, STATIC_ROOT, STATIC_URL, MEDIA_ROOT, MEDIA_URL
+from manage_party_service.settings.base import get_environment_var
 import sentry_sdk
+from django.views.static import serve
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,8 +20,7 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-# todo: sentry not working
-if not settings.DEBUG and get_environment_var('SENTRY_DNS'):
+if not settings.DEBUG:
     sentry_sdk.init(
         get_environment_var('SENTRY_DNS'),
         # Set traces_sample_rate to 1.0 to capture 100%
@@ -38,5 +38,9 @@ urlpatterns = [
 urlpatterns += [path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                 path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), ]
 
-urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
-urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^api/static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^api/media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT})
+]
