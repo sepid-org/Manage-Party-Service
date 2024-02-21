9ead94ad71099c41fdeb54c811cbf945067b56a0
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-21 03:45:30 +0330

    fix OpenGraphMetaData serializer

--- apps/site_appearance/serializers/og_metadata.py
+++ apps/site_appearance/serializers/og_metadata.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from apps.site_appearance.models import OpenGraphMetaData
+from apps.site_appearance.serializers.logo_serializer import get_media_url
 
 
 class OpenGraphMetadataSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = OpenGraphMetaData
-        fields = ['title', 'type', 'image', 'url']
+        fields = '__all__'
+
+    def get_image(self, obj):
+        return get_media_url() + obj.image.url
