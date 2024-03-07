6782600fb0f2a0b999c8355efb4dc9f989136bd6
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-03-07 18:22:23 +0330

    init third_party app

---
+++ apps/third_party/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class SMSService(models.Model):
+    # مثل کاوه‌نگار
+    pass
+
+
+class VoiceCallService(models.Model):
+    # مثل کاوه‌نگار
+    pass
+
+
+class PaymentService(models.Model):
+    # مثل زرین‌پال
+    pass
+
+
+class SiteSupportService(models.Model):
+    # مثل گفتینو
+    pass
