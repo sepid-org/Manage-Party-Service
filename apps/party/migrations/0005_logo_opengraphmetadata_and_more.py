ab24e87a74251d14180959fbfa2d732c3b6b6470
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-03-10 05:12:06 +0330

    fix migration file

--- apps/party/migrations/0005_logo_opengraphmetadata_and_more.py
+++ apps/party/migrations/0005_logo_opengraphmetadata_and_more.py
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='party',
             name='og_metadata',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.PROTECT, related_name='party', to='party.opengraphmetadata'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='party', to='party.opengraphmetadata', null=True, blank=True),
             preserve_default=False,
         ),
         migrations.AlterField(
